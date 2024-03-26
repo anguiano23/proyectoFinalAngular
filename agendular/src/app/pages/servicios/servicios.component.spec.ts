9d1e70a4dd1afbd79e44016ec955da4c8e4b2068
jaggalvan <jaggalvan@hotmail.com>
2024-03-25 20:35:58 -0600

    servicios3

--- agendular/src/app/pages/servicios/servicios.component.spec.ts
+++ agendular/src/app/pages/servicios/servicios.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ServiciosComponent } from './servicios.component';
 
 describe('ServiciosComponent', () => {
@@ -8,16 +7,18 @@ describe('ServiciosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ServiciosComponent]
+      declarations: [ ServiciosComponent ]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ServiciosComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('crear', () => {
     expect(component).toBeTruthy();
   });
 });
